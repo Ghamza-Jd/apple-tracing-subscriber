39ee83a23b2dba887fe1630fed93d96ed264caee
Hamza Jadid <haljadid@proximie.com>
2024-02-19 10:25:18 +0200

    feat: added tracing enabled impl

--- src/ats.rs
+++ src/ats.rs
@@ -1,13 +1,25 @@
+use crate::apple_log::AppleLog;
+use dashmap::DashMap;
 use tracing_core::span;
 use tracing_core::Event;
+use tracing_core::Level;
 use tracing_core::Metadata;
 use tracing_core::Subscriber;
 
-pub struct AppleTracingSubscriber;
+pub struct AppleTracingSubscriber {
+    subsystem: String,
+    categories: DashMap<String, (Option<Level>, AppleLog)>,
+}
 
 impl Subscriber for AppleTracingSubscriber {
     fn enabled(&self, metadata: &Metadata<'_>) -> bool {
-        todo!()
+        let max_level = self
+            .categories
+            .get(metadata.target())
+            .and_then(|pair| pair.0)
+            .unwrap_or_else(|| Level::TRACE);
+
+        metadata.level() <= &max_level
     }
 
     fn new_span(&self, span: &span::Attributes<'_>) -> span::Id {
@@ -32,5 +44,14 @@ impl Subscriber for AppleTracingSubscriber {
 
     fn exit(&self, span: &span::Id) {
         todo!()
+    }
+}
+
+impl AppleTracingSubscriber {
+    pub fn new(subsystem: &str) -> Self {
+        Self {
+            subsystem: subsystem.to_string(),
+            categories: DashMap::new(),
+        }
     }
 }
